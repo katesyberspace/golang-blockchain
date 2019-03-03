3f0d1eecc844a174bc4851fd4343ed5bef7ca430
Kate Sy <ksy@assemblypayments.com>
2019-03-03 21:05:10 +1100

    added blockchain folder

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/katesyberspace/golang-blockhain
+
+require (
+	github.com/Pallinder/go-randomdata v1.1.0
+	github.com/misocactus/golang-blockchain v0.0.0-20190303084145-3d5e592ee29e
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/misocactus/golang-blockchain/blockchain"
+	"strconv"
+)
+
+func main(){
+	fmt.Println("hello world -- golang blockchain starts now")
+
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+
+	fmt.Print(chain)
+}
